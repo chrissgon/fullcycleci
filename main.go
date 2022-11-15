367b52f6942b0c366fe69cdf4b7c98cddcb336f7
Christopher Gonçalves <christopher.goncalves2002@hotmail.com.br>
2022-11-15 07:00:44 -0300

    feat: :sparkles: uncomment methods

--- main.go
+++ main.go
@@ -22,34 +22,34 @@ func div(a int, b int) int {
 	return a / b
 }
 
-// func diva(a int, b int) int {
-// 	return a / b
-// }
+func diva(a int, b int) int {
+	return a / b
+}
 
-// func divb(a int, b int) int {
-// 	return a / b
-// }
+func divb(a int, b int) int {
+	return a / b
+}
 
-// func divc(a int, b int) int {
-// 	return a / b
-// }
+func divc(a int, b int) int {
+	return a / b
+}
 
-// func divd(a int, b int) int {
-// 	return a / b
-// }
+func divd(a int, b int) int {
+	return a / b
+}
 
-// func dive(a int, b int) int {
-// 	return a / b
-// }
+func dive(a int, b int) int {
+	return a / b
+}
 
-// func divf(a int, b int) int {
-// 	return a / b
-// }
+func divf(a int, b int) int {
+	return a / b
+}
 
-// func divg(a int, b int) int {
-// 	return a / b
-// }
+func divg(a int, b int) int {
+	return a / b
+}
 
-// func divh(a int, b int) int {
-// 	return a / b
-// }
+func divh(a int, b int) int {
+	return a / b
+}
